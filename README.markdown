57149434bb31cd265678c1d18962838c9e8524af
Mark Watkinson <markwatkinson@gmail.com>
2011-09-09 23:12:20 +0100

    first commit

---
+++ README.markdown
@@ -0,0 +1,39 @@
+# External script/CSS loading library for CodeIgniter
+
+Simple library to manage/schedule the loading of script and CSS files from your
+controllers.
+
+Example Usage:
+
+Suppose relative to the top level index.php that your scripts exist in a
+directory called 'assets/scripts/', and CSS in 'assets/style'.
+
+Suppose your stylesheet is called 'style.css', and you want to load a copy of
+jQuery.
+
+In your controller:
+
+```php
+<?php
+
+$this->load->library('scripts');
+// sets the path
+$this->scripts->set_dir('css', 'assets/style');
+$this->scripts->set_dir('js', 'assets/scripts');
+
+$this->scripts->js('jquery-1.6'); // this is globbed against *jquery-1.6*.js
+$this->scripts->css('style.css', true); // the second argument tells us that it
+                // is an exact file pattern, i.e. no globbing
+```
+
+
+Then in your view:
+
+```php
+<head>
+<?= $this->scripts->html(); ?>
+</head>
+```
+
+Note that the html function can take a single argument which if set to TRUE will
+output the contents of the included files, instead of using src/href attributes.
---
+++ libraries/Scripts.php
@@ -0,0 +1,135 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Script/CSS includes made a little easier
+ *
+ * Load the library then use js() and cs() to schedule JavaScript and CSS
+ * includes, then from your view, echo the html() function's output.
+ */
+class Scripts {
+  
+  /// script (js) directory
+  public $script_dir = 'assets/script';
+  /// css directory
+  public $css_dir = 'assets/css';
+
+  /**
+   * URL prefix
+   * when files are included with their src/href attributes, the prefix is
+   * pre-pended to their addresses
+   * This defaults to base_url().
+   */
+  public $prefix = '';
+
+  /// JS to be loaded
+  private $scripts = array();
+  /// CSS to be loaded
+  private $css = array();
+
+  public function Scripts() {
+    if (!function_exists('base_url')) {
+      $CI = &get_instance();
+      $CI->load->helper('url');
+    }
+    $this->prefix = base_url();
+  }
+
+  private function _set_script_dir($dir) {
+    $this->script_dir = $dir;
+  }
+  private function _set_css_dir($dir) {
+    $this->css_dir = $dir;
+  }
+
+  /**
+   * Sets the directory to use for script/CSS includes
+   * @param type 'js' or 'css'
+   * @param $dir the diretory to use
+   * @throws InvalidArgumentException if $type is neither 'css' nor 'js'
+   * @warning Don't use absolute paths - use the path relative to the CWD
+   * (which should be where your index.php is)
+   *
+   * TODO: Allow absolute paths, involves some trickery in figuring out where
+   * the document root is relative to it.
+   */
+  public function set_dir($type, $dir) {
+    if ($type === 'js') _set_script_dir($dir);
+    elseif ($type === 'css') _set_css_dir($dir);
+    else throw new InvalidArgumentException("Invalid type: $type");
+  }
+  
+  /**
+   * Schedules loading of a script/CSS file,
+   * @param $type 'js' or 'css'
+   * @param $name script/CSS filename
+   * @param $exact If FALSE, $name is used in a wildcard
+   * @returns TRUE if the file is found and included, otherwise FALSE
+   *
+   * @throws InvalidArgumentException if $type is neither 'css' nor 'js'
+   */
+  private function _load_file($type, $name, $exact) {
+    if (!in_array($type, array('js', 'css')))
+      throw new InvalidArgumentException("Invalid type: $type");
+    $search_path = ($type === 'js')? $this->script_dir : $this->css_dir;
+    $matches = array();
+    if (!$exact) {
+      $matches = glob($search_path . "/*$name*.$type");
+    } else {
+      $path = $search_path . "/$name.$type";
+      if (file_exists())
+        $matches[] = $path;
+    }
+    if (empty($matches)) return false;
+    else {
+      if ($type === 'js') $this->scripts = array_merge($this->scripts, $matches);
+      else $this->css = array_merge($this->css, $matches);
+      return true;
+    }
+  }
+  
+  /**
+   * Schedules a JavaScript include
+   * @param $name the filename
+   * @param $exact If FALSE, $name is used in a wildcard
+   * @returns TRUE if the file is found and included, otherwise FALSE   *
+   */
+  public function js($name, $exact=false) {
+    return $this->_load_file('js', $name, $exact);
+  }
+
+  /**
+   * Schedules a CSS include
+   * @param $name the filename
+   * @param $exact If FALSE, $name is used in a wildcard
+   * @returns TRUE if the file is found and included, otherwise FALSE   *
+   */
+  public function css($name, $exact=false) {
+    return $this->_load_file('css', $name, $exact);
+  }
+
+  /**
+   * Gets the HTML to include the scheduled files
+   * @param $concat If TRUE, the files are read and their contents written to
+   * the page directly, otherwise the script/CSS tags' src and href attributes
+   * are set to point to the files
+   * @return A string of HTML to go in the head tag
+   */
+  public function html($concat = false) {
+    $output = '';
+    foreach($this->scripts as $s) {
+      $output .= "<script type='text/javascript'";
+      if ($concat) $output .= '>' . file_get_contents($s);
+      else $output .= " src='{$this->prefix}/$s'>";
+      $output .= '</script>';
+      $output .= "\n";
+    }
+    foreach($this->css as $c) {
+      if ($concat) 
+        $output .= "<style type='text/css'>" . file_get_contents($c) . "</style>";
+      else
+        $output .= "<link rel='stylesheet' type='text/css' href='{$this->prefix}/$c'>";
+      $output .= "\n";
+    }
+    return $output;
+  }
+}
